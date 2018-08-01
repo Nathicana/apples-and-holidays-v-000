12eb3d89711905acf061d3bf629dc993cc8d1b94
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-01 01:44:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
 
-      puts "#{season.to_s.capitalize!}: #{holiday.to_s.capitalize!}: #{supplies.to_s.split.collect(&:capitalize).join("")}"
+      puts "#{season.to_s.capitalize!}": 
+      puts #{holiday.to_s.capitalize!}: #{supplies.to_s.split.collect(&:capitalize).join("")}"
 
 
 
